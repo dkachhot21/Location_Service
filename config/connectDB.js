073281998ab7ccf77df4900ec334e193c9f088e3
Ketan Sharma <81684683+sharma01ketan@users.noreply.github.com>
2024-05-27 10:51:47 +0530

    await lagao bhaiya ji

--- config/connectDB.js
+++ config/connectDB.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        const connect = mongoose.connect(process.env.DB_CONNECTION_STRING);
+        const connect = await mongoose.connect(process.env.DB_CONNECTION_STRING);
         console.log("MongoDB Connected", (await connect).connection.host);
     } catch (err) {
         console.log(err);
